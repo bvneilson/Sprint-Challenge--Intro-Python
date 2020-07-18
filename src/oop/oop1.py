86ca48e7519880200e694ad417659d6c0b335b1d
Brennan Neilson <bvneilson@gmail.com>
2020-07-17 20:12:52 -0600

    class hierarchy complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# main base class
+class Vehicle():
+    pass
+
+# base class for Car and Motorcycle
+class GroundVehicle(Vehicle):
+    pass
+
+# base class for Airplane and Starship
+class FlightVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
